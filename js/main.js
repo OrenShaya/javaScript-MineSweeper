7ec33e9530d17db812e691353af82337ae0556ef
Oren Shaya <58634407+OrenShaya@users.noreply.github.com>
2024-12-19 15:44:24 +0200

    So much is working
    Safe first click - works
    recursive opening on empty - works
    marking - works

--- js/main.js
+++ js/main.js
@@ -6,21 +6,32 @@
  *  cols - number of columns
  *  mines - to count the mines
  *  minesLoc - an array with {i: i, j: j} of mine locations
+ *  lifes - number of lifes
  * 
  * gBoard a 2D array with the board backend
  *  each cell has:
- *      status: 'open' or 'unopend'
+ *      status: 'open' or 'unopend' or 'marked'
  *      content: MINE or CLEAR
+ * 
+ * stuff TODO:
+ *  recursive safe opening
+ *   
+ *  add sound
+ *  
  */
 
 const MINE = '💣'
-const CLEAR = '🕳' // '🐾🏞💧🕳🔘🔵⬛⬜'
+const CLEAR = '🔵' // '🐾🏞💧🕳🔘⬛⬜'
+const FLAG = '🚩'
+const LIFE = '❤️'
 
 const gGame = {}
 const gBoard = []
 
 function onInit(elBtn) {
     gGame.status = 'playing'
+    gGame.lifes = 3
+    gGame.firstClick = true
 
     // empty in case left from prev game
     while (gBoard.length > 0) {
@@ -73,10 +84,6 @@ function buildBoard(rows, cols, mines) {
         gBoard.push(row)
     }
     gGame.mines = mines
-    gGame.minesLoc = createMinesLocations()
-    placeMines()
-
-    countMines()
 }
 
 function renderBoard() {
@@ -89,7 +96,10 @@ function renderBoard() {
 
             const cell = gBoard[i][j]
 
-            strHTML += `<td onclick="onCellClick(this)" class="cell ${cell.status}" data-location="cell-${i}-${j}">${cell.content}</td>`
+            strHTML += `<td onclick="onCellClick(this)" 
+                    oncontextmenu="onCellMarked(this)"
+                    class="cell ${cell.status}" 
+                    data-location="cell-${i}-${j}"></td>`
         }
         strHTML += '</tr>'
     }
@@ -97,7 +107,19 @@ function renderBoard() {
     
     const elContainer = document.querySelector('.board')
     elContainer.innerHTML = strHTML
+    // stops right click default function
+    elContainer.addEventListener("contextmenu",function(e) {e.preventDefault()});
 
+    // add life
+    const elLifeText = document.querySelector('.life-text')
+    elLifeText.innerText = 'Lives: '
+    updateLifes()
+
+    // add mines counter
+    const elMineCounterText = document.querySelector('.mines-counter-text')
+    elMineCounterText.innerText = 'Mines left:'
+    var elMineCounterNum = document.querySelector('.mines-counter-num')
+    elMineCounterNum.innerText = gGame.mines
 }
 
 function onCellClick(elCell) {
@@ -105,21 +127,79 @@ function onCellClick(elCell) {
 
     // cell-2-4
     var loc = elCell.dataset.location.split('-')
-    loc = {i: loc[1], j: loc[2]}
+    loc = {i: +loc[1], j: +loc[2]}
     elCell.classList.remove('unopened')
     elCell.classList.add('opened')
+    
+    if (gGame.firstClick) { 
+        gGame.minesLoc = createMinesLocations(loc)
+        placeMines()
+        countMines()
+        gGame.firstClick = false
+    }
+
+    showCell(loc)
 
     gBoard[loc.i][loc.j].status = 'opened'
 
     if (gBoard[loc.i][loc.j].content === MINE) {
-        gameOver('lose')
+        gGame.lifes--
+        updateLifes()
+        if (gGame.lifes === 0) gameOver('lose')
+    } else if (gBoard[loc.i][loc.j].content === 0) {
+        openNegs(loc)
     }
-    else {
-        if (checkWin()) gameOver('win')
+    if (checkWin()) gameOver('win')
+}
+
+function onCellMarked(elCell) {
+    var loc = elCell.dataset.location.split('-')
+    loc = {i: loc[1], j: loc[2]}
+    
+    var mineDiff
+    if (gBoard[loc.i][loc.j].status === 'marked') {
+        // unmark it
+        elCell.innerText = ''
+        
+        mineDiff = 1
+
+        gBoard[loc.i][loc.j].status = 'unopened'
+    }   // mark it
+    else if (gBoard[loc.i][loc.j].status === 'unopened') {
+        elCell.innerText = FLAG
+        gBoard[loc.i][loc.j].status = 'marked'
+        mineDiff = -1
+    }
+    else return
+    var elMineCounter = document.querySelector('.mines-counter-num')
+    elMineCounter.innerText = +elMineCounter.innerText + mineDiff
+}
+
+function openNegs(loc) {
+    // console.log('origin:', loc.i, loc.j, gBoard[loc.i][loc.j].content)
+    for (let i = loc.i - 1; i <= loc.i + 1; i++) {
+        if (i < 0 || i >= gGame.rows) continue
+        
+        for (let j = loc.j - 1; j <= loc.j + 1; j++) {
+            if (j < 0 || j >= gGame.cols || (j == loc.j && i == loc.i)) continue         
+            // console.log('neighbours i, j: ', i, j, 'origin:', loc.i, loc.j)                
+            if (gBoard[i][j].status === 'unopened') {
+                onCellClick(document.querySelector(`[data-location="cell-${i}-${j}"]`))
+            }
+        }
+    }
+}
+
+function updateLifes() {
+    var elLifeSymbol = document.querySelector('.life-symbol')
+    elLifeSymbol.innerText = ''
+    for (let i = 0; i < gGame.lifes; i++) {
+        elLifeSymbol.innerText += LIFE + ' '
     }
 }
 
 function gameOver(state) {  // state = 'win' / 'lose'
+    revelAll()
     gGame.status = 'game over'
     var text
     if (state === 'lose') {
@@ -136,10 +216,36 @@ function gameOver(state) {  // state = 'win' / 'lose'
 function checkWin() {
     for (let i = 0; i < gGame.rows; i++) {
         for (let j = 0; j < gGame.cols; j++) {
-            if (gBoard[i][j].status === 'unopened' && gBoard[i][j].content !== MINE) return false
+            // if   !== opened: mark or unopened   and not a mine (number or empty)
+            if ((gBoard[i][j].status !== 'opened') && gBoard[i][j].content !== MINE) {
+                return false
+            }
         }
     }
     return true
+}
+
+function revelAll(isDevMode = false) { 
+    for (let i = 0; i < gBoard.length; i++) {
+        for (let j = 0; j < gBoard[i].length; j++) {
+            const elCell = document.querySelector(`[data-location="cell-${i}-${j}"]`)
+            if (!isDevMode) {
+                elCell.classList.remove('unopened')
+                elCell.classList.add('opened')
+            }
+            showCell({i: i, j: j})
+        }
+    }
+}
+
+function showCell(loc) { // NOTE: does not update status!
+    const elCell = document.querySelector(`[data-location="cell-${loc.i}-${loc.j}"]`)
+    if (gBoard[loc.i][loc.j].content !== 0) {
+        elCell.innerText = gBoard[loc.i][loc.j].content
+    }
+    else {
+        elCell.innerText = CLEAR
+    }
 }
 
 function countMines() {
@@ -180,11 +286,11 @@ function createCell() {
     return {status: 'unopened', content: CLEAR}
 }
 
-function createMinesLocations() {
+function createMinesLocations(firstClickLoc) {
     var minesLoc = []
     for (let i = 0; i < gGame.mines; i++) {
         var loc = generateRandomLocation(gGame.rows, gGame.cols)
-        while (isMineTaken(loc, minesLoc)) {
+        while (isMineTaken(loc, minesLoc) || (loc.i == firstClickLoc.i && loc.j == firstClickLoc.j)) {
             loc = generateRandomLocation(gGame.rows, gGame.cols)
         }
         minesLoc.push(loc)
